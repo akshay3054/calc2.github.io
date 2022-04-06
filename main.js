d58cabbcbe084767ca7b1dde365d23d563da66a2
nishant0811 <renegade.plays03@gmail.com>
2022-04-06 18:04:56 +0530

    first push

---
+++ main.js
@@ -0,0 +1,423 @@
+let luxury ={
+  fabric : 1200,
+  foam : 2500,
+  paddingFoam : 440,
+  fevicol : 300,
+  staplerPin : 120,
+  nails : 80,
+  thread : 80,
+  jute : 250,
+  bottomFabric : 60,
+  plywood : 1800,
+  packinigMaterialCost : 500,
+  additionalCarpenterPayments : 1000,
+  helper : 500,
+  inboundLogistics : 500,
+  outboundLogistics : 800,
+  labour : 1200,
+  salesInstallation : 1200,
+  partnerMaterialMargin : 0,
+  ucComissionLabour : 15,
+  ucComissionMaterial : 15
+}
+
+let premium = {
+  fabric : 1200,
+  foam : 1671,
+  paddingFoam : 440,
+  fevicol : 300,
+  staplerPin : 120,
+  nails : 80,
+  thread : 80,
+  jute : 250,
+  bottomFabric : 60,
+  plywood : 1800,
+  packinigMaterialCost : 500,
+  additionalCarpenterPayments : 1000,
+  helper : 500,
+  inboundLogistics : 500,
+  outboundLogistics : 800,
+  labour : 1200,
+  salesInstallation : 1200,
+  partnerMaterialMargin : 0,
+  ucComissionLabour : 15,
+  ucComissionMaterial : 15
+}
+
+let extraComforatble = {
+  fabric : 600,
+  foam : 1671,
+  paddingFoam : 440,
+  fevicol : 300,
+  staplerPin : 120,
+  nails : 80,
+  thread : 80,
+  jute : 250,
+  bottomFabric : 60,
+  plywood : 1800,
+  packinigMaterialCost : 500,
+  additionalCarpenterPayments : 1000,
+  helper : 500,
+  inboundLogistics : 500,
+  outboundLogistics : 800,
+  labour : 1200,
+  salesInstallation : 1200,
+  partnerMaterialMargin : 0,
+  ucComissionLabour : 15,
+  ucComissionMaterial : 15
+}
+
+let standard ={
+  fabric : 600,
+  foam : 454,
+  paddingFoam : 440,
+  fevicol : 300,
+  staplerPin : 120,
+  nails : 80,
+  thread : 80,
+  jute : 250,
+  bottomFabric : 60,
+  plywood : 1800,
+  packinigMaterialCost : 500,
+  additionalCarpenterPayments : 1000,
+  helper : 500,
+  inboundLogistics : 500,
+  outboundLogistics : 800,
+  labour : 1200,
+  salesInstallation : 1200,
+  partnerMaterialMargin : 0,
+  ucComissionLabour : 15,
+  ucComissionMaterial : 15
+}
+
+let budget = {
+  fabric : 400,
+  foam : 454,
+  paddingFoam : 440,
+  fevicol : 300,
+  staplerPin : 120,
+  nails : 80,
+  thread : 80,
+  jute : 150,
+  bottomFabric : 60,
+  plywood : 1800,
+  packinigMaterialCost : 500,
+  additionalCarpenterPayments : 1000,
+  helper : 500,
+  inboundLogistics : 500,
+  outboundLogistics : 800,
+  labour : 1000,
+  salesInstallation : 1000,
+  partnerMaterialMargin : 0,
+  ucComissionLabour : 20,
+  ucComissionMaterial : 20
+}
+
+function calculate(){
+
+  let materialFabricNeeded = parseFloat(document.getElementById("mof").value);
+  let seatThickness = parseFloat(document.getElementById("sThick").value);
+  let noOfFoamSheets = parseFloat(document.getElementById("nofs").value);
+  let paddingThickness = parseFloat(document.getElementById("pThick").value);
+  let noOfFoamSheetsPadding = parseFloat(document.getElementById("nfsp").value);
+
+  let seatFoam = noOfFoamSheets*seatThickness
+  let paddingFoam = paddingThickness * noOfFoamSheetsPadding;
+
+  let fevicol=2,
+  staplerPin=1,
+  nails =1,
+  thread = 1,
+  jute = 2,
+  bottomFabric = 2,
+  packinigMaterialCost = 1,
+  plywood = materialFabricNeeded/8*3;
+
+  let additionalCarpenterPayments =0;
+  let helper = 0;
+  let inboundLogistics =1;
+  let outboundLogistics = 1;
+  let salesInstallation = 0.25
+  let labour = 1+(materialFabricNeeded/5-1)*0.25
+
+  helper = labour
+
+  if(materialFabricNeeded != 0 ){
+    let data = 2 + (materialFabricNeeded/5-1);
+    fevicol = data;
+    jute = data;
+    bottomFabric = data
+  }
+
+  //Repair Cost
+
+  //Luxury Calculation
+
+  let luxMatRep =
+  materialFabricNeeded*luxury.fabric+
+  seatFoam * luxury.foam +
+  paddingFoam * luxury.paddingFoam +
+  fevicol * luxury.fevicol +
+  staplerPin * luxury.staplerPin+
+  nails * luxury.nails +
+  thread * luxury.thread +
+  jute * luxury.jute +
+  bottomFabric * luxury.bottomFabric
+
+  let luxPatRep =
+  labour * luxury.labour+
+  salesInstallation * luxury.salesInstallation
+
+  let luxRepTotal =
+  luxMatRep + luxPatRep +
+  (luxMatRep * luxury.ucComissionMaterial / 100) +
+  (luxPatRep * luxury.ucComissionLabour / 100)
+
+  document.getElementById("luxrepair").innerHTML = Math.round(luxRepTotal)
+
+  //Premium Fabric Calculation
+
+  let preMatRep =
+  materialFabricNeeded*premium.fabric+
+  seatFoam * premium.foam +
+  paddingFoam * premium.paddingFoam +
+  fevicol * premium.fevicol +
+  staplerPin * premium.staplerPin+
+  nails * premium.nails +
+  thread * premium.thread +
+  jute * premium.jute +
+  bottomFabric * premium.bottomFabric
+
+  let prePatRep =
+  labour * premium.labour+
+  salesInstallation * premium.salesInstallation
+
+  let preRepTotal =
+  preMatRep + prePatRep +
+  (preMatRep * premium.ucComissionMaterial / 100) +
+  (prePatRep * premium.ucComissionLabour / 100)
+
+  document.getElementById("pfrepair").innerHTML = Math.round(preRepTotal)
+
+  //Extra Comfortable Calculation
+
+  let ecMatRep =
+  materialFabricNeeded*extraComforatble.fabric+
+  seatFoam * extraComforatble.foam +
+  paddingFoam * extraComforatble.paddingFoam +
+  fevicol * extraComforatble.fevicol +
+  staplerPin * extraComforatble.staplerPin+
+  nails * extraComforatble.nails +
+  thread * extraComforatble.thread +
+  jute * extraComforatble.jute +
+  bottomFabric * extraComforatble.bottomFabric
+
+  let ecPatRep =
+  labour * extraComforatble.labour+
+  salesInstallation * extraComforatble.salesInstallation
+
+  let ecRepTotal =
+  ecMatRep + ecPatRep +
+  (ecMatRep * extraComforatble.ucComissionMaterial / 100) +
+  (ecPatRep * extraComforatble.ucComissionLabour / 100)
+
+  document.getElementById("ecomfrepair").innerHTML = Math.round(ecRepTotal)
+
+  // Standard Calculation
+
+  let standMatRep =
+  materialFabricNeeded*standard.fabric+
+  seatFoam * standard.foam +
+  paddingFoam * standard.paddingFoam +
+  fevicol * standard.fevicol +
+  staplerPin * standard.staplerPin+
+  nails * standard.nails +
+  thread * standard.thread +
+  jute * standard.jute +
+  bottomFabric * standard.bottomFabric
+
+  let standPatRep =
+  labour * standard.labour+
+  salesInstallation * standard.salesInstallation
+
+  let standRepTotal =
+  standMatRep + standPatRep +
+  (standMatRep * standard.ucComissionMaterial / 100) +
+  (standPatRep * standard.ucComissionLabour / 100)
+
+  document.getElementById("standrepair").innerHTML = Math.round(standRepTotal)
+
+  // Budget Calculation
+
+  let budMatRep =
+  materialFabricNeeded*budget.fabric+
+  seatFoam * budget.foam +
+  paddingFoam * budget.paddingFoam +
+  fevicol * budget.fevicol +
+  staplerPin * budget.staplerPin+
+  nails * budget.nails +
+  thread * budget.thread +
+  jute * budget.jute +
+  bottomFabric * budget.bottomFabric
+
+  let budPatRep =
+  labour * budget.labour+
+  salesInstallation * budget.salesInstallation
+
+  let budRepTotal =
+  budMatRep + budPatRep +
+  (budMatRep * budget.ucComissionMaterial / 100) +
+  (budPatRep * budget.ucComissionLabour / 100)
+
+
+  document.getElementById("budrepair").innerHTML = Math.round(budRepTotal);
+
+
+
+  //New Price Calculation
+
+  //Luxury
+
+  let luxAddLabCost =
+  additionalCarpenterPayments * luxury.additionalCarpenterPayments +
+  helper * luxury.helper
+
+  let luxLogisCost =
+  inboundLogistics * luxury.inboundLogistics +
+  outboundLogistics * luxury.outboundLogistics;
+
+  let luxPatNew =
+  labour * luxury.labour * 1.15 +
+  salesInstallation * luxury.salesInstallation;
+
+  let luxTotal =
+  luxAddLabCost +
+  luxLogisCost +
+  luxPatNew +
+  (luxPatRep * luxury.ucComissionLabour /100) +
+  (luxMatRep * luxury.ucComissionMaterial / 100);
+
+  luxMatRep +=
+  plywood * luxury.plywood +
+  packinigMaterialCost * luxury.packinigMaterialCost
+
+  luxTotal += luxMatRep
+
+  document.getElementById("luxnew").innerHTML = Math.round(luxTotal);
+
+  //Premium Fiber
+
+  let preAddLabCost =
+  additionalCarpenterPayments * premium.additionalCarpenterPayments +
+  helper * premium.helper
+
+  let preLogisCost =
+  inboundLogistics * premium.inboundLogistics +
+  outboundLogistics * premium.outboundLogistics;
+
+  let prePatNew =
+  labour * premium.labour * 1.15 +
+  salesInstallation * premium.salesInstallation;
+
+  let preTotal =
+  preAddLabCost +
+  preLogisCost +
+  prePatNew +
+  (prePatRep * premium.ucComissionLabour /100) +
+  (preMatRep * premium.ucComissionMaterial / 100);
+
+  preMatRep +=
+  plywood * premium.plywood +
+  packinigMaterialCost * premium.packinigMaterialCost
+
+  preTotal += preMatRep
+
+  document.getElementById("pfnew").innerHTML = Math.round(preTotal);
+
+  //Extra Comfortable
+
+  let ecAddLabCost =
+  additionalCarpenterPayments * extraComforatble.additionalCarpenterPayments +
+  helper * extraComforatble.helper
+
+  let ecLogisCost =
+  inboundLogistics * extraComforatble.inboundLogistics +
+  outboundLogistics * extraComforatble.outboundLogistics;
+
+  let ecPatNew =
+  labour * extraComforatble.labour * 1.15 +
+  salesInstallation * extraComforatble.salesInstallation;
+
+  let ecTotal =
+  ecAddLabCost +
+  ecLogisCost +
+  ecPatNew +
+  (ecPatRep * extraComforatble.ucComissionLabour /100) +
+  (ecMatRep * extraComforatble.ucComissionMaterial / 100);
+
+  ecMatRep +=
+  plywood * extraComforatble.plywood +
+  packinigMaterialCost * extraComforatble.packinigMaterialCost
+
+  ecTotal += ecMatRep
+
+  document.getElementById("ecomfnew").innerHTML = Math.round(ecTotal);
+
+  //Standard
+
+  let standAddLabCost =
+  additionalCarpenterPayments * standard.additionalCarpenterPayments +
+  helper * standard.helper
+
+  let standLogisCost =
+  inboundLogistics * standard.inboundLogistics +
+  outboundLogistics * standard.outboundLogistics;
+
+  let standPatNew =
+  labour * standard.labour * 1.15 +
+  salesInstallation * standard.salesInstallation;
+
+  let standTotal =
+  standAddLabCost +
+  standLogisCost +
+  standPatNew +
+  (standPatRep * standard.ucComissionLabour /100) +
+  (standMatRep * standard.ucComissionMaterial / 100);
+
+  standMatRep +=
+  plywood * standard.plywood +
+  packinigMaterialCost * standard.packinigMaterialCost
+
+  standTotal += standMatRep
+
+  document.getElementById("standnew").innerHTML = Math.round(standTotal);
+
+  //Budget
+  let budAddLabCost =
+  additionalCarpenterPayments * budget.additionalCarpenterPayments +
+  helper * budget.helper
+
+  let budLogisCost =
+  inboundLogistics * budget.inboundLogistics +
+  outboundLogistics * budget.outboundLogistics;
+
+  let budPatNew =
+  labour * budget.labour * 1.15 +
+  salesInstallation * budget.salesInstallation;
+
+  let budTotal =
+  budAddLabCost +
+  budLogisCost +
+  budPatNew +
+  (budPatRep * budget.ucComissionLabour /100) +
+  (budMatRep * budget.ucComissionMaterial / 100);
+
+  budMatRep +=
+  plywood * budget.plywood +
+  packinigMaterialCost * budget.packinigMaterialCost
+
+  budTotal += budMatRep
+
+  document.getElementById("budnew").innerHTML = Math.round(budTotal);
+
+}
